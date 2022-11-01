481e27e8782bc3596b43f57110c1f49ec71ab729
Michael Reed <18372368+chakravala@users.noreply.github.com>
2022-11-01 18:08:52 -0400

    revised vim, i3, juliastartup

--- .julia/config/startup.jl
+++ .julia/config/startup.jl
@@ -1,19 +1,46 @@
+
+#        /\
+#       /  \     ___   ___   __  __ _   _
+#      / /\ \   / _ \ / __) /  \/ /| | | |
+#     / /__\ \ | |_) )> _) ( ()  < | |_| |
+#    /________\|  __/ \___) \__/\_\| ._,_|
+#  ______      | |                 | |
+#  \  ___)     |_|                 |_|
+#   \ \   __   __  __  __ ___  ___  ___   ___
+#    > >  \ \ / / /  \/ /(   )(   )/ __) / _ \
+#   / /__  \ v / ( ()  <  | |  | | > _) | |_) )
+#  /_____)  > <   \__/\_\  \_)  \_)\___)|  __/
+#          / ^ \                        | |
+#         /_/ \_\                       |_|
+#
+#   https://crucialflow.com
+#   https://github.com/chakravala
+#       _           _                         _
+#      | |         | |                       | |
+#   ___| |__   __ _| | ___ __ __ ___   ____ _| | __ _
+#  / __| '_ \ / _` | |/ / '__/ _` \ \ / / _` | |/ _` |
+# | (__| | | | (_| |   <| | | (_| |\ V / (_| | | (_| |
+#  \___|_| |_|\__,_|_|\_\_|  \__,_| \_/ \__,_|_|\__,_|
+
+ENV["JULIA_PKG_SERVER"] = "pkg.julialang.org"
 ENV["JULIA_NUM_THREADS"] = 8
+ENV["SIMILITUDE"] = 1
 ENV["AUTHOR"] = "Michael Reed"
 ENV["EDITOR"] = "vim"
 ENV["PYTHON"] = "python"
 ENV["REDPRE"] = "0"
+ENV["VIEWER"] = "zathura"
+ENV["UNITDOCS"] = "1"
 #using Pkg
 #using OhMyREPL
 #using UnicodePlots #, ImageInTerminal
-using BenchmarkTools, SyntaxTree, VerTeX
-#using Reduce
-#@force using Reduce.Algebra
-#using Grassmann
+using BenchmarkTools, SyntaxTree #, VerTeX
+#using Reduce #; @force using Reduce.Algebra
+import Base: @pure
 
-#=cdpkg(pkg) = cd(dirname(Base.find_package(string(pkg))))
+cdpkg(pkg) = cd(dirname(Base.find_package(string(pkg))))
 
 macro cdpkg(pkg)
     cdpkg(pkg)
     return nothing
-end=#
+end
--- .vimrc
+++ .vimrc
@@ -22,27 +22,36 @@ set tabstop=4 shiftwidth=4
 autocmd FileType julia set expandtab softtabstop=4
 
 colo slate
+hi Normal guibg=NONE ctermbg=NONE
 autocmd FileType tex colorscheme lucius
 autocmd FileType tex hi Normal guibg=NONE ctermbg=NONE
 autocmd FileType tex hi LineNr guibg=NONE ctermbg=NONE
 autocmd FileType tex exec("setlocal dictionary+=".$HOME."/.vim/dictionaries/".expand('<amatch>'))
 
+let g:tex_flavor = 'latex'
 let g:vimtex_matchparen_enabled = 0
-let g:vimtex_view_method = 'my_zathura'
+let g:vimtex_view_method = 'zathura'
 "let g:vimtex_view_general_viewer = 'qpdfview'
 "let g:vimtex_view_general_options = '--unique @pdf\#src:@tex:@line:@col'
 "let g:vimtex_view_general_options_latexmk = '--unique'
 let g:vimtex_view_general_options_latexmk = '-pdflatex=lualatex'
+
 
 "let g:vimtex_view_automatic = 0
 let g:vimtex_view_forward_search_on_start = 0
 let g:vimtex_view_process_start = 0
 let g:vimtex_view_xwin_exists = 0
 
+autocmd BufNewFile,BufRead *.wl set syntax=wl
+autocmd BufNewFile,BufRead *.wls set syntax=wl
+autocmd BufNewFile,BufRead *.m set syntax=wl
+
 autocmd FileType julia hi DiffAdd guifg=NONE ctermfg=NONE guibg=#464632 ctermbg=238 gui=NONE cterm=NONE
 autocmd FileType julia hi DiffChange guifg=NONE ctermfg=NONE guibg=#335261 ctermbg=239 gui=NONE cterm=NONE
 autocmd FileType julia hi DiffDelete guifg=#f43753 ctermfg=203 guibg=#79313c ctermbg=237 gui=NONE cterm=NONE
 autocmd FileType julia hi DiffText guifg=NONE ctermfg=NONE guibg=NONE ctermbg=NONE gui=reverse cterm=reverse
+
+"autocmd FileType julia let g:slime_target = "x11"
 
 " julia
 "autocmd FileType julia let g:default_julia_version = '1.0'
